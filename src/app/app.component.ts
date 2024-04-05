72fd93093ad3a1a59e0bd060561438201b3ae682
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-04-05 23:49:56 +0500

    Started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'LeetCodeProject';
+  title = 'leetcodeExampleOne';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
